2b04828c80daef9978b11e7bfece47bcd0af61b0
Madhu Korada <manoj.korada0000@gmail.com>
2023-03-07 03:23:07 -0500

    Build succeeding

--- src/motion_decoder/src/motion_decoder.cpp
+++ src/motion_decoder/src/motion_decoder.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <motion_decoder/image_converter.hpp>
-#include <apriltags_ros/AprilTagDetectionArray.h>
-#include <apriltags_ros/AprilTagDetection.h>
+#include <apriltag_ros/AprilTagDetectionArray.h>
+#include <apriltag_ros/AprilTagDetection.h>
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Quaternion.h>
@@ -9,25 +9,52 @@
 
 ImageConverter* ic;
 
-void apriltag_detection_callback(const apriltags_ros::AprilTagDetectionArray msg)
+class RosClass
 {
-  ROS_INFO("In subscribe\n");
-  //TODO: Parse message and publish transforms as apriltag_tf and camera
+public:
+	RosClass();
+	~RosClass();
+	void apriltag_detection_callback(const apriltag_ros::AprilTagDetectionArray msg);
+private:
+	ros::NodeHandle nh_;
+	ros::Subscriber apriltag_detection_sub_;  
+};
+
+RosClass::RosClass()
+{
+	//TODO: Add a subscriber to get the AprilTag detections The callback function skelton is given.
+	apriltag_detection_sub_ = nh_.subscribe("/tag_detections", 1, &RosClass::apriltag_detection_callback, this);
+	ros::spin();
+	ros::Rate loop_rate(50);
+	while(ros::ok()) {
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+}
+
+RosClass::~RosClass()
+{
+	ROS_INFO("In destructor\n");
+}
+void RosClass::apriltag_detection_callback(const apriltag_ros::AprilTagDetectionArray msg)
+{
+	ROS_INFO("In subscribe\n");
+	//TODO: Parse message and publish transforms as apriltag_tf and camera
+  
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
-  
-  ros::NodeHandle n;
-  //TODO: Add a subscriber to get the AprilTag detections The callback function skelton is given.
-  ImageConverter converter;
-  ic = &converter;
-  ros::Rate loop_rate(50);
-  ROS_INFO("In main\n");
-  while(ros::ok()) {
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  return 0;
+	ros::init(argc, argv, "image_converter");
+	RosClass rosclass;
+
+	ImageConverter converter;
+	ic = &converter;
+	ROS_INFO("In main\n");
+	// ros::Rate loop_rate(50);
+	// while(ros::ok()) {
+	// 	ros::spinOnce();
+	// 	loop_rate.sleep();
+	// }
+	return 0;
 }
